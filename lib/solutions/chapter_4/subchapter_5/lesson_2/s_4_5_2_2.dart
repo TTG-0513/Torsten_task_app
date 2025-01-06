d578da38214473806fde99349d2f7f8b1126aa34
TTG-0513 <viper1305@freenet.de>
2025-01-06 13:49:17 +0100

    fast fertig

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -4,6 +4,15 @@ class S4522 extends StatelessWidget {
   const S4522({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Text("Button drücken");
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hallo Torsten");
   }
 }
